cc7323004bd4b6892e1ad7be8e2ac3a2647c66f4
Thomas Boers <thomasboers@hotmail.com>
2017-03-24 16:19:11 +0100

    Prepare for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to:, 'cocktails#index'
+  root to:'cocktails#index'
 
   resources :cocktails, only: [:index, :new, :create, :show] do
   resources :doses, only: [:new, :create]
