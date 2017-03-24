da67a47053128f8e5f2eb222985ebf5cdb5ab7d0
Thomas Boers <thomasboers@hotmail.com>
2017-03-24 16:35:13 +0100

    photo presence

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+   config.cloud_name = 'dl2mshsvd'
+   config.api_key = '136161159176118'
+   config.api_secret = 'ouHy82FGJUyAE-Ua5DLf50v-TEg'
+   config.cdn_subdomain = true
+ end
