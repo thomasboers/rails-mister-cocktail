f862fec7f24cf1501d11f1439f55548ee54a9da4
Thomas Boers <thomasboers@hotmail.com>
2017-03-24 16:02:25 +0100

    heroku fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to:, 'cocktails#index'
+
   resources :cocktails, only: [:index, :new, :create, :show] do
   resources :doses, only: [:new, :create]
   end
